c423d9d1492e585bd6df0a7a95624d559153389e
Alex Cevallos <cevalloa@gmail.com>
2015-03-28 16:27:54 -0400

    Added Project

---
+++ Hack NJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hack NJ
+//
+//  Created by Alex Cevallos on 3/28/15.
+//  Copyright (c) 2015 AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hack NJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hack NJ
+//
+//  Created by Alex Cevallos on 3/28/15.
+//  Copyright (c) 2015 AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
